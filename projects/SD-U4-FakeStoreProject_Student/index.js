74b62fb922670a98083ef591d0dfcc308a5d8454
miloKoleske <mckoleske@gmail.com>
2023-11-24 12:15:20 -0700

    Fake Store Progress

--- projects/SD-U4-FakeStoreProject_Student/index.js
+++ projects/SD-U4-FakeStoreProject_Student/index.js
@@ -1,1 +1,36 @@
 // everything except body (nav, display, footer) need to have prevent default action to display in flex
+
+// API includes updating products to carts, getting a user's cart, etc.
+
+// ! Global Variables
+// ! API url needs value
+const apiURL = '...'; 
+let userCart = [];
+
+// ! DOM Elements
+const category = document.querySelector('btn btn-outline-success me-2')
+console.log(category);
+
+// ! Functions
+// TODO: Display Single Card
+
+let card = document.createElement('display');
+let
+
+// TODO: Display Cart
+
+
+
+
+
+
+
+
+
+
+// ! Event Listeners
+
+
+fetch(baseURL)
+            .then(res=>res.json())
+            .then(json=>console.log(json))
